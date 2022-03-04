15e44d8a8b737ef5c1e7271115c9c7b3fe1b88ce
Ryosuke-Shigi <adalovelace6365@gmail.com>
2022-03-04 16:54:56 +0900

    test

--- app/Http/Controllers/lineController.php
+++ app/Http/Controllers/lineController.php
@@ -46,7 +46,35 @@ class lineController extends Controller
 
             return 'ok';
         } */
+
+        // LINEBOTSDKの設定
+        // LINEから送られた内容を$inputsに代入
+        $inputs=$request->all();
+
+        // そこからtypeをとりだし、$message_typeに代入
+        $message_type=$inputs['events'][0]['type'];
+
+        // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
+        if($message_type=='message') {
+
+            // replyTokenを取得
+            $reply_token=$inputs['events'][0]['replyToken'];
+
+            // LINEBOTSDKの設定
+            $http_client = new CurlHTTPClient(config('services.line.accessToken'));
+            $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.channelSecret')]);
+
+            // 送信するメッセージの設定
+            $reply_message='メッセージありがとうございます';
+
+            // ユーザーにメッセージを返す
+            $reply=$bot->replyText($reply_token, $reply_message);
+
+            return 'ok';
+        }
+
+
         //return response()->json(['mytle'=>'mytle'],200);
-        return response()->json($request->all());
+        //return response()->json($request->all());
     }
 }
