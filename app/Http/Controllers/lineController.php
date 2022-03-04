339dc10abd3a40f19bd6ace695662a4dd6c47bae
Ryosuke-Shigi <adalovelace6365@gmail.com>
2022-03-04 18:18:21 +0900

    replytext

--- app/Http/Controllers/lineController.php
+++ app/Http/Controllers/lineController.php
@@ -22,35 +22,11 @@ class lineController extends Controller
     }
 
     public function mytles(REQUEST $request){
-/*         // LINEから送られた内容を$inputsに代入
-        $inputs=$request->all();
-
-        // そこからtypeをとりだし、$message_typeに代入
-        $message_type=$inputs['events'][0]['type'];
-
-        // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
-        if($message_type=='message') {
-
-            // replyTokenを取得
-            $reply_token=$inputs['events'][0]['replyToken'];
-
-            // LINEBOTSDKの設定
-            $http_client = new CurlHTTPClient(config('services.line.accessToken'));
-            $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.channelSecret')]);
-
-            // 送信するメッセージの設定
-            $reply_message='メッセージありがとうございます';
-
-            // ユーザーにメッセージを返す
-            $reply=$bot->replyText($reply_token, $reply_message);
-
-            return 'ok';
-        } */
 
         // LINEBOTSDKの設定
         // LINEから送られた内容を$inputsに代入
         //return json_decode(json_encode($request->all()),true);
-        $inputs=json_decode(json_encode($request->all()),true);
+/*         $inputs=json_decode(json_encode($request->all()),true);
 
         // そこからtypeをとりだし、$message_typeに代入
         $message_type=$inputs['events'][0]['type'];
@@ -65,7 +41,6 @@ class lineController extends Controller
             $http_client = new CurlHTTPClient(config('services.line.accessToken'));
             $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.channelSecret')]);
 
-            return $http_client;
             // 送信するメッセージの設定
             $reply_message='メッセージありがとうございます';
 
@@ -74,10 +49,26 @@ class lineController extends Controller
 
             return 'ok';
         }
+ */
 
-        //return config('services.line.accessToken');
-        //return config('services.line.channelSecret');
-        //return response()->json(['mytle'=>'mytle'],200);
-        //return response()->json($request->all());
+        $channel_secret = env('LINE_CHANNEL_SECRET');
+        $access_token = env('LINE_ACCESS_TOKEN');
+
+        // replyTokenを取得
+        $inputs=json_decode(json_encode($request->all()),true);
+        $reply_token=$inputs['events'][0]['replyToken'];
+
+        $client = new CurlHTTPClient($access_token);
+        $bot = new LINEBot($client, ['channelSecret' => $channel_secret]);
+
+        $bot->replytext($reply_token,"こんにちわ！");
+
+        return $reply_token;
+
+
     }
+
+
+
+
 }
