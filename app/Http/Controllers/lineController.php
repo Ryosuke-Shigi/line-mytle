deb13e729289958f4c296626abd6a512d64372fb
Ryosuke-Shigi <adalovelace6365@gmail.com>
2022-03-04 15:38:38 +0900

    line sdk etc re install

--- app/Http/Controllers/lineController.php
+++ app/Http/Controllers/lineController.php
@@ -5,6 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 //use \Symfony\Component\HttpFoundation\Response;//webステータスコード
 
+/* LINE関係 */
+use Illuminate\Support\Str;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+
+
 class lineController extends Controller
 {
     //
