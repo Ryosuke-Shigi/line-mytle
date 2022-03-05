2d0af19d7a7310402d95338a8414fc1ca984abbe
Ryosuke-Shigi <adalovelace6365@gmail.com>
2022-03-05 15:54:03 +0900

    test

--- app/Http/Controllers/lineController.php
+++ app/Http/Controllers/lineController.php
@@ -50,11 +50,9 @@ class lineController extends Controller
 
         //メッセージ送信
         if($voice == ""){
-            $bot->replytext($reply_token,$voice.$inputs['events'][0]['message']['text']." なんだなっ！");
-            return $voice.$inputs['events'][0]['message']['text']." なんだなっ！";
+            $bot->replytext($reply_token,$inputs['events'][0]['message']['text']." なんだなっ！");
         }else{
             $bot->replytext($reply_token,$voice);
-            return $voice;
         }
 
         return 0;
